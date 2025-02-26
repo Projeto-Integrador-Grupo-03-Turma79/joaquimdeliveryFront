6e9e43e667debc09bc87802b99a0ab59afe6eaec
Lucas da Silva Pimentel <pimentellucas753@gmail.com>
2025-02-26 14:26:22 -0300

    home

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/joaquimdelilogo2.svg"
+
+export default function Navbar()  {
+
+    function saida(){
+        alert('Obrigado por usar o nosso sistema!');
+    }
+
+    return (
+        <>
+        <nav className='w-full bg-[#700300] text-white flex justify-center py-4'>
+            <div className="container flex justify-between text-2xl">
+                <Link to='/home' > <img src={logo} alt="Logo de RH" className="w-25" /> </Link>
+
+                <div className='flex gap-20 items-center justify-center'>
+                    <Link to='/sobre' className='hover:underline'>Sobre nós</Link>
+                    <Link to='/restaurantes' className='hover:underline'>Restaurantes</Link>
+                    <a onClick={saida} href="https:/google.com" className='hover:underline'> Sair </a>
+                </div>
+            </div>
+        </nav>
+    </>
+    )
+}
